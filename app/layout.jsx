806f81b9d664df3c16dcdc5c17228e60a3ff1617
Julian Sanz <jjulisanz@gmail.com>
2024-02-13 19:54:50 -0300

    metadata update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,11 @@
 import "./globals.css";
 import { Providers } from "./providers";
 
-
 export const metadata = {
-  title: "ValoDesignWeb",
-  description: "Select Agent Valorant Web",
+  title: "Valorant Select Agent Web",
+  description:
+    "This project is a dynamic web application built with Next.js, Tailwind CSS, and JavaScript, offering an immersive experience reminiscent of the agent selection process in the popular game Valorant. Users can explore a visually appealing interface where they can select agents, each with unique abilities, mimicking the game's agent selection feature.",
+  metadataBase: new URL("https://agent-selector.vercel.app/"),
 };
 
 export default function RootLayout({ children }) {
